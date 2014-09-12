24ee5d2a8c7feff20e5650eeb529b70e58d62a28
n3l5 <n3l5@users.noreply.github.com>
2014-09-11 21:04:05 -0500

    initial

--- irmempull.ps1
+++ irmempull.ps1
@@ -1,10 +1,65 @@
 ﻿<#  
 .SYNOPSIS  
-    
+    IR Forensic MEMory pull (irMEMpull)
+
 .DESCRIPTION
+irMEMpull is a PowerShell script utilized to pull dump memory from a live WinXP-Win7 system on your network. It DOES NOT utilize WinRM capabilities.
 
+Utilizes the winpmem memory dumping tool to dump the memory. When done collecting the artifacts, it will 7zip the data and pull the info off the box for offline analysis. 
+
+.PARAMETER Target
+    This is the target computer where you will be collecting artifacts from.
+
+.PARAMETER ToolsDir
+	This the file path location of the tools on the analysis system.
+
+.PARAMETER DumpDir
+	This is the file path location you want the memory dumped. (On analysis system or other location like UNC path to server share)
+
+.PARAMETER 7zpass
+	This is the password for the compressed & password protected file that the artifacts will be put into.
+
+.NOTEs:  
+    
+	All testing done on PowerShell v4
+		
+	Requires winpmem.exe for memory acquisition.
+	Requires 7za.exe (7zip cmd line) for compression w/ password protection
+	
+	Assumed Directories:
+	c:\windows\temp\IR - Where the work will be done/copied
+		
+	Must be ran as a user that will have Admin creds on the remote system.
+	
+    LINKs:  
+	
+	irMEMpull main - https://github.com/n3l5/irMempull
+	
+	Links to required tools:
+	winpmem - Memory acquisition tool - can be downloaded from: http://sourceforge.net/projects/volatility.mirror/files/
+	7-Zip - Part of the 7-Zip archiver - can be downloaded from: http://www.7-zip.org/
+		
+	Various tools for analysis of the artifacts:
+	Volatility - https://github.com/volatilityfoundation
+	Mandiant Redline - https://www.mandiant.com/resources/download/redline
+	
 #>
-
+#>
+Param(
+  [Parameter(Mandatory=$True,Position=0)]
+   [string]$target,
+   
+   [Parameter(Mandatory=$True)]
+   [string]$toolsDir,
+   
+   [Parameter(Mandatory=$True)]
+   [string]$dumpDir,
+   
+   [Parameter(Mandatory=$True)]
+   [string]$7zpass
+   
+    )
+   
 echo "=============================================="
 echo "=============================================="
 Write-Host -Fore Magenta "
@@ -19,11 +74,11 @@ Write-Host -Fore Magenta "
                               |_|              
 
  "
+echo ""
 echo "=============================================="
 Write-Host -Fore Yellow "Run as administrator/elevated privileges!!!"
 echo "=============================================="
 echo ""
-
 
 Write-Host -Fore Cyan ">>>>> Press a key to begin...."
 [void][System.Console]::ReadKey($TRUE)
@@ -31,26 +86,56 @@ echo ""
 echo ""
 $userDom = Read-Host "Enter your target DOMAIN (if any)..."
 $username = Read-Host "Enter you UserID..."
-$combCred = "$userDom" + "\$username"
-$cred = Get-Credential $combCred
-$target = read-host ">>>>> Please enter a HOSTNAME or IP..."
-$irFolder = "c:\Windows\Temp\IR\"
+$domCred = "$userDom" + "\$username"
+$compCred = "$target" + "\$username"
+#Fill credentials based on whether domain or remote system credentials used 
+if (!($userDom)){
+	$cred = Get-Credential $compCred
+	}
+else {
+	$cred = Get-Credential $domCred
+	}
 echo ""
-Write-Host -Fore Yellow ">>>>> pinging $target...."
+
+#Test if the box is up and running
+
+Write-Host -Fore Yellow ">>>>> Testing connection to $target...."
 echo ""
-c:\TOOLS\tcping.exe -s -i 10 -r 10 $target 445
+if ((!(Test-Connection -Cn $target -BufferSize 16 -Count 1 -ea 0 -quiet)) -OR (!($socket = New-Object net.sockets.tcpclient("$target",445)))) {
+	Write-Host -Foreground Magenta "$target appears to be down"
+	}
+
+################
+#Target is up start the collection
+################
+
+else {
+
+$startTime = Get-Date -format yyyy-MM-dd_HHmm
+Write-host -Foregroundcolor Cyan "-[ Start time: $startTime ]-"
+
+#Determine if Mail Alert is wanted ask for particulars
+if ($mail -like "Y*") {
+	$mailTo = Read-Host "Enter alert TO: email address...multiples should separated with a comma"
+	$mailFrom = Read-Host "Enter alert FROM: email address..."
+	$smtpServer = Read-Host "Enter SMTP relay server..."
+	}
+elseif ((!($mail)) -OR ($mail -like "N*")) {
+	Write-Host -Foregroundcolor Cyan "  -Mail notification off-"
+	}
+
+#Get system info
+	$targetName = Get-WMIObject Win32_ComputerSystem -ComputerName $target -Credential $cred | ForEach-Object Name
+	$targetIP = Get-WMIObject -Class Win32_NetworkAdapterConfiguration -ComputerName $target -Credential $cred -Filter "IPEnabled='TRUE'" | Where {$_.IPAddress} | Select -ExpandProperty IPAddress | Where{$_ -notlike "*:*"}
+	$mem = Get-WMIObject -class Win32_PhysicalMemory -ComputerName $target -Credential $cred | Measure-Object -Property capacity -Sum | % {[Math]::Round(($_.sum / 1GB),2)} 
+	$expproc = gwmi win32_process -computer $target -Credential $cred -Filter "Name = 'explorer.exe'"
+	$exuser = ($expproc.GetOwner()).user
+	$exdom = ($expproc.GetOwner()).domain
+	$currUser = "$exdom" + "\$exuser"
+
 echo ""
 echo "=============================================="
-
-$targetName = Get-WMIObject Win32_ComputerSystem -ComputerName $target -Credential $cred | ForEach-Object Name
-$targetIP = Get-WMIObject -Class Win32_NetworkAdapterConfiguration -ComputerName $target -Filter "IPEnabled='TRUE'" | Where {$_.IPAddress} | Select -ExpandProperty IPAddress | Where{$_ -notlike "*:*"}
 Write-Host -ForegroundColor Magenta "==[ $targetName - $targetIP ]=="
-
-################
-##Set up environment on remote system. IR folder for tools and art folder for artifacts.##
-################
-##For consistency, the working directory will be located in the "c:\windows\temp\IR" folder on both the target and initiator system.
-##Tools will stored directly in the "IR" folder for use. Artifacts collected on the local environment of the remote system will be dropped in the workingdir.
 
 ##Determine x32 or x64
 $arch = Get-WmiObject -Class Win32_Processor -ComputerName $target -Credential $cred | foreach {$_.AddressWidth}
@@ -63,67 +148,79 @@ $OSvers = Get-WMIObject -Class Win32_OperatingSystem -ComputerName $target -Cred
 	if ($OSvers -like "6*"){
 	Write-Host -ForegroundColor Magenta "==[ Host OS: Windows 7 $arch    ]=="
 	}
-echo "=============================================="
-echo ""
+	Write-Host -ForegroundColor Magenta "==[ "Total memory size: $mem GB" ]=="
+	Write-Host -ForegroundColor Magenta "==[ Current user: $currUser ]=="
+
+	echo "=============================================="
+	echo ""
+
+################
+##Set up environment on remote system. IR folder for memtools and art folder for memory.##
+################
+##For consistency, the working directory will be located in the "c:\windows\temp\IR" folder on both the target and initiator system.
+##Tools will stored directly in the "IR" folder for use. Artifacts collected on the local environment of the remote system will be dropped in the workingdir.
+
+
 ##Set up PSDrive mapping to remote drive
-New-PSDrive -Name X -PSProvider filesystem -Root \\$target\c$ -Credential $cred | Out-Null
-
-$remoteIRfold = "X:\windows\Temp\IR"
-$date = Get-Date -format yyyy-MM-dd_HHmm_
-$irFolder = "c:\Windows\Temp\IR\"
-$artFolder = $date + $targetName
-$workingDir = $irFolder + $artFolder
-$dirList = ("$remoteIRfold\$artFolder")
-New-Item -Path $dirList -ItemType Directory | Out-Null
-
+	New-PSDrive -Name X -PSProvider filesystem -Root \\$target\c$ -Credential $cred | Out-Null
+    New-Item -Path $remoteMEMfold -ItemType Directory | Out-Null
+	$irFolder = "C:\Windows\Temp\IR"
+	$remoteMEMfold = "X:\windows\Temp\IR"
+	$date = Get-Date -format yyyy-MM-dd_HHmm_
+	
 ##connect and move software to target client
-Write-Host -Fore Green "Copying tools...."
-$tool = "C:\TOOLS\winpmem\winpmem_1.4.exe"
-Copy-Item $tool $remoteIRfold -recurse
+	Write-Host -Fore Green "Copying tools...."
+	Copy-Item $toolsDir\*.exe $remoteMEMfold -recurse
+	Write-Host -ForegroundColor Yellow "  [done]"
 
-$Memdump = "cmd /c c:\windows\temp\ir\winpmem_1.4.exe c:\windows\temp\ir\physmem.raw"
-InVoke-WmiMethod -class Win32_process -name Create -ArgumentList $memdump -ComputerName $target -Credential $cred | Out-Null
-do {(Write-Host -ForegroundColor Yellow "  memory copy to complete..."),(Start-Sleep -Seconds 30)}
-until ((Get-WMIobject -Class Win32_process -Filter "Name='winpmem_1.4.exe'" -ComputerName $target -Credential $cred | where {$_.Name -eq "winpmem_1.4.exe"}).ProcessID -eq $null)
-}
-Write-Host "  [done]"
+#Run MEMDUMP remote
+	$memName = $date + $targetName + "_memDump.7z"
+	$dumpPath = $irFolder+"\"+$memName
+	$memdump = "cmd /c $irFolder\winpmem.exe - | $irFolder\7za.exe a -si -p$passwd -mx1 $dumpPath"
+	InVoke-WmiMethod -class Win32_process -name Create -ArgumentList $memdump -ComputerName $target -Credential $cred | Out-Null
+	echo "=============================================="
+	Write-Host -ForegroundColor Magenta ">>>[Memory acquisition started]<<<"
+	echo "=============================================="
+
+	#Monitor the Winpmem process
+do {(Write-Host -ForegroundColor Yellow "dumping the memory - compressing image..."),(Start-Sleep -Seconds 180)}
+until ((Get-WMIobject -Class Win32_process -Filter "Name='winpmem.exe'" -ComputerName $target -Credential $cred | where {$_.Name -eq "winpmem.exe"}).ProcessID -eq $null)
+Write-Host -ForegroundColor Yellow "  [done]"
 
 ###################
 ##Package up the data and pull
 ###################
 echo ""
 echo "=============================================="
-Write-Host -Fore Magenta ">>>[Packaging the collection]<<<"
+Write-Host -Fore Magenta ">>>[Transferring the image...]<<<"
 echo "=============================================="
 echo ""
 
-##7zip the artifact collection##
-$passwd = read-host ">>>>> Please supply a password"
-$7z = "cmd /c c:\Windows\temp\IR\7za.exe a $workingDir.7z -p$passwd -mhe $workingDir_memdump -y > null"
-InVoke-WmiMethod -class Win32_process -name Create -ArgumentList $7z -ComputerName $target -Credential $cred | Out-Null
-do {(Write-Host -ForegroundColor Yellow "  packing the collected artifacts..."),(Start-Sleep -Seconds 10)}
-until ((Get-WMIobject -Class Win32_process -Filter "Name='7za.exe'" -ComputerName $target -Credential $cred | where {$_.Name -eq "7za.exe"}).ProcessID -eq $null)
-Write-Host -ForegroundColor Yellow "  Packing complete..."
-
 ##size it up
-Write-Host -ForegroundColor Cyan "  [Package Stats]"
-$dirsize = "{0:N2}" -f ((Get-ChildItem $remoteIRfold\$artFolder | Measure-Object -property length -sum ).Sum / 1MB) + " MB"
-Write-Host -ForegroundColor Cyan "  Working Dir: $dirsize "
-$7zsize = "{0:N2}" -f ((Get-ChildItem $remoteIRfold\$artfolder.7z | Measure-Object -property length -sum ).Sum / 1MB) + " MB"
-Write-Host -ForegroundColor Cyan "  Package size: $7zsize "
+$remdumpPath = $remoteMEMfold+"\"+$memName
+$7zsize = "{0:N2}" -f ((Get-ChildItem $remdumpPath | Measure-Object -property length -sum ).Sum / 1GB) + " GB"
+Write-Host -ForegroundColor Cyan "  Image size: $7zsize "
 
-Write-Host -Fore Green "Transfering the package...."
-Move-Item $remoteIRfold\$artfolder_memdump.7z $irFolder
+Write-Host -Fore Green "Transfering the image...."
+if (!(Test-Path -Path $irFolder -PathType Container)){
+	New-Item -Path $irFolder -ItemType Directory  | Out-Null
+}
+
+Move-Item $remdumpPath $dumpDir
 Write-Host -Fore Yellow "  [done]"
 
-###Delete the IR folder##
-Write-Host -Fore Green "Removing the working environment...."
-Remove-Item $remoteIRfold -Recurse -Force 
+###Delete the remote IR folder 7 tools##
+Write-Host -Fore Green "Removing the remote working environment...."
+Remove-Item $remoteMEMfold -Recurse -Force 
 
 ##Disconnect the PSDrive X mapping##
 Remove-PSDrive X
 
 ##Ending##
+$endTime = Get-Date -format yyyy-MM-dd_HHmm
+Write-Host -Foregroundcolor Cyan "-[ End time: $endTime ]-"
 echo "=============================================="
-Write-Host -ForegroundColor Magenta ">>>>>>>>>>[[ irMemPull complete ]]<<<<<<<<<<<"
+Write-Host -ForegroundColor Magenta ">>>>>>>>>>[ irMemPull complete ]<<<<<<<<<<<"
 echo "=============================================="
+}
+
